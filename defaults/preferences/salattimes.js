2b7ea8a283fbfbfa6d03ae24f09d251aabfe31a9
mainulhossain <mainulhossain@ab8e532c-e571-11de-a3ff-fbe938f12fc4>
2010-01-02 13:33:38 +0000

    adding contents

---
+++ defaults/preferences/salattimes.js
@@ -0,0 +1,6 @@
+pref("extensions.salattimes.location", "");
+pref("extensions.salattimes.boolpref", false);
+pref("extensions.salattimes.intpref", 0);
+pref("extensions.salattimes.stringpref", "Karlsruhe|BW|Germany|8|49");
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.salattimes@mainulhossain.description", "chrome://salattimes/locale/salattimes.properties");
